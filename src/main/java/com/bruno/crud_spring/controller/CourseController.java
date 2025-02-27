d611f639c5f8a8b44123bd4d7c0bdc42418ed587
Bruno Cardoso de Menezes <bmenezes@dnways.com>
2025-02-27 18:53:01 -0300

    Implementação do método Salvar

--- src/main/java/com/bruno/crud_spring/controller/CourseController.java
+++ src/main/java/com/bruno/crud_spring/controller/CourseController.java
@@ -2,8 +2,15 @@ package com.bruno.crud_spring.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bruno.crud_spring.model.Course;
@@ -22,5 +29,14 @@ public class CourseController {
     @GetMapping
     public List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus
+    public ResponseEntity<Course> create(@RequestBody @Validated Course course) {
+        //System.out.println(course.getName());
+        //return courseRepository.save(course);
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(courseRepository.save(course));
     }
 }
